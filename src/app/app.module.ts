511f67f48acd3eb03f1cbdc220e2e882d7658ca6
Jefferson Oliveira <jefferson.admsistemas@gmail.com>
2024-03-06 13:04:27 -0300

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxMatTuiCalendarModule } from 'ngx-mat-tui-calendar';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -17,6 +18,7 @@ import { SharedModule } from './shared/shared.module';
     SharedModule,
     AppRoutingModule,
     CoreModule,
+    NgxMatTuiCalendarModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/shared/components/calendario/calendario.component.ts
+++ src/app/shared/components/calendario/calendario.component.ts
@@ -1,7 +1,31 @@
 import { Component } from '@angular/core';
+import { CalendarOptions } from 'ngx-mat-tui-calendar';
+import { IOptions, ISchedule, IWeekOptions } from 'tui-calendar';
 
 @Component({
   selector: 'app-calendar',
   templateUrl: './calendario.component.html',
 })
-export class CalendarioComponent {}
+export class CalendarioComponent {
+  options: CalendarOptions = {
+    ioptions: {
+      defaultView: 'month',
+      taskView: true,
+      month: {
+        narrowWeekend: false,
+      },
+      week: {
+        narrowWeekend: false,
+        hourStart: 7,
+        hourEnd: 20,
+      } as IWeekOptions,
+    } as IOptions,
+  } as CalendarOptions;
+
+  // in lieu of a back-end database, a Map is used for demo purposes
+  schedules: Map<string, ISchedule> = new Map();
+
+  onUserCreatedSchedule($event: any) {
+    console.log($event);
+  }
+}
